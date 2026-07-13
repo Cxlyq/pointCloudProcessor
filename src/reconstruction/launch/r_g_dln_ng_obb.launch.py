2d52aaf86b7424a2c9dd0b1b1ab7d4846354daad
Xu Chen <cx3348269780@outlook.com>
2026-07-13 11:16:31 +0800

    feat(r-dln): 新增dln地面重建算法

---
+++ src/reconstruction/launch/r_g_dln_ng_obb.launch.py
@@ -0,0 +1,27 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    package_name = 'reconstruction'
+
+    config_file_path = os.path.join(
+        get_package_share_directory(package_name),
+        'config',
+        'r_config.yaml'
+    )
+
+    # 启动新的混合重建节点
+    reconstruction_g_dln_ng_obb_node = Node(
+        package=package_name,
+        executable='reconstruction_g_dln_ng_obb_node', # 需在 CMakeLists 中配置同名执行文件
+        name='reconstruction_g_dln_ng_obb_node',       # 对应 yaml 中的新建命名空间
+        parameters=[config_file_path],
+        output='screen',
+        emulate_tty=True
+    )
+
+    return LaunchDescription([
+        reconstruction_g_dln_ng_obb_node
+    ])
